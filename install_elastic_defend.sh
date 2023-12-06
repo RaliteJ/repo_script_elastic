e0a5c50d1709a8eec4cabfe61e217ba9513800a3
Justin Ralite <74457153+RaliteJ@users.noreply.github.com>
2023-12-06 10:30:11 +0100

    Update install_elastic_defend.sh

--- install_elastic_defend.sh
+++ install_elastic_defend.sh
@@ -2,7 +2,7 @@
 #Take 2 variables:
 #$1: id of the user
 #$2: pass of elastic
-curl --cacert /opt/siem/ca.crt -k --user elastic:nylDWmG367XBj4Vn+ZUO --request POST \
+curl --cacert /opt/siem/ca.crt -k --request POST \
   --url 'https://10.202.0.180:5601/api/fleet/package_policies' \
   -H 'Accept: */*' \
   -H 'Accept-Language: en-US,en;q=0.9' \
