2f2cf22990d31fd457556c3f58d9830b64c767f0
Justin Ralite <74457153+RaliteJ@users.noreply.github.com>
2023-12-08 11:28:17 +0100

    Update modif_make_pass.sh

--- modif_make_pass.sh
+++ modif_make_pass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "echo ${ELASTIC_PASSWORD} > /opt/siem/repo_script_elastic/pass_elastic" >> /opt/siem/scripts/print_password.sh
+echo {"echo ${ELASTIC_PASSWORD} > /opt/siem/repo_script_elastic/pass_elastic"} >> /opt/siem/scripts/print_password.sh
 
 exit 0
