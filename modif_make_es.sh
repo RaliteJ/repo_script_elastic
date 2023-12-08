6d2ffc9599591cfbc990e97cbd801fcc79acf0ec
Justin Ralite <74457153+RaliteJ@users.noreply.github.com>
2023-12-08 10:05:12 +0100

    Update modif_make_es.sh

--- modif_make_es.sh
+++ modif_make_es.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sed -i '/^echo "test de ES"$/ { N; s/make curlES\n\n\n#sudo chown -R root.root $(pwd)\/config$/exit 0/ }' /opt/siem/scripts/lance-ES.sh
-
+sed -i '/^echo "test de ES"$/ { N; s/make curlES$/exit 0/ }' /opt/siem/scripts/lance-ES.sh
 
 exit 0
