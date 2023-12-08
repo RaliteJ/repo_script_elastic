b11269eb643022812a632cee8a99a436c1e6cd4d
Justin Ralite <74457153+RaliteJ@users.noreply.github.com>
2023-12-08 11:38:19 +0100

    Update create_new_policies.sh

--- create_new_policies.sh
+++ create_new_policies.sh
@@ -3,8 +3,8 @@
 #$1 is the id of the policy
 #$2 is the password of elastic
 curl --cacert /opt/siem/ca.crt -k  -X POST "https://10.202.0.180:5601/api/fleet/agent_policies?sys_monitoring=true" --header 'kbn-xsrf: true' --header 'Content-Type: application/json'  --data-raw '{
-  "name": "$1",
-  "id": "$1",
+  "name": "'$1'",
+  "id": "'$1'",
   "description": "",
   "namespace": "default",
   "monitoring_enabled": [
